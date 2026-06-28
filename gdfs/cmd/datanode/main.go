c1cfc0cdb448c1794fe09c29978fa50fe8362728
storious <2227563488@qq.com>
2026-06-28 17:52:27 +0800

    feat(gdfs): add datanode HTTP server

--- gdfs/cmd/datanode/main.go
+++ gdfs/cmd/datanode/main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"gdfs/internal/datanode"
+)
+
+func main() {
+	var (
+		id   = flag.String("id", "node-1", "datanode id")
+		addr = flag.String("addr", ":9001", "listen address")
+		root = flag.String("root", "data/datanode", "storage root")
+	)
+	flag.Parse()
+
+	store := datanode.NewLocalBlockStore(*root)
+
+	node, err := datanode.NewDataNode(
+		datanode.NodeID(*id),
+		*addr,
+		store,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := datanode.NewHTTPServer(node)
+
+	log.Printf("starting datanode id=%s addr=%s root=%s", *id, *addr, *root)
+
+	if err := http.ListenAndServe(*addr, server); err != nil {
+		log.Fatal(err)
+	}
+}
