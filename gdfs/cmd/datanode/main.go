fbbbbf7518cdeaccb13b0cb850b114e3644052cc
storious <2227563488@qq.com>
2026-06-29 19:43:07 +0800

    feat(gdfs): send periodic datanode heartbeat

--- gdfs/cmd/datanode/main.go
+++ gdfs/cmd/datanode/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"time"
 
 	"gdfs/internal/cluster"
 	"gdfs/internal/datanode"
@@ -32,19 +33,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if *namenodeAddr != "" {
-		metaClient := namenode.NewHTTPClient(*namenodeAddr)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-		err := metaClient.Heartbeat(context.Background(), cluster.Heartbeat{
-			ID:       cluster.DataNodeID(*id),
-			Addr:     "http://localhost" + *addr,
-			Capacity: *capacity,
-			Used:     0,
-		})
-		if err != nil {
-			log.Printf("heartbeat failed: %v", err)
-		}
+	hb := cluster.Heartbeat{
+		ID:       cluster.DataNodeID(*id),
+		Addr:     "http://localhost" + *addr,
+		Capacity: *capacity,
+		Used:     0,
 	}
+
+	startHeartbeat(ctx, *namenodeAddr, hb, 5*time.Second)
 
 	server := datanode.NewHTTPServer(node)
 
@@ -53,4 +52,36 @@ func main() {
 	if err := http.ListenAndServe(*addr, server); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func startHeartbeat(ctx context.Context, namenodeAddr string, hb cluster.Heartbeat, interval time.Duration) {
+	if namenodeAddr == "" {
+		return
+	}
+
+	client := namenode.NewHTTPClient(namenodeAddr)
+
+	go func() {
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
+
+		send := func() {
+			if err := client.Heartbeat(ctx, hb); err != nil {
+				log.Printf("heartbeat failed: %v", err)
+				return
+			}
+			log.Printf("heartbeat sent id=%s namenode=%s", hb.ID, namenodeAddr)
+		}
+
+		send()
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				send()
+			}
+		}
+	}()
 }
