6128f1408005e40b7f3bea1d42157fcf071547cd
storious <2227563488@qq.com>
2026-06-29 20:20:15 +0800

    chore(gdfs): fix format for cluster/errors.go

--- gdfs/internal/cluster/errors.go
+++ gdfs/internal/cluster/errors.go
@@ -3,8 +3,8 @@ package cluster
 import "errors"
 
 var (
-	ErrEmptyDataNodeID   = errors.New("empty datanode id")
-	ErrEmptyDataNodeAddr = errors.New("empty datanode address")
+	ErrEmptyDataNodeID      = errors.New("empty datanode id")
+	ErrEmptyDataNodeAddr    = errors.New("empty datanode address")
 	ErrNoAliveDataNodes     = errors.New("no alive datanodes")
 	ErrNotEnoughDataNodes   = errors.New("not enough datanodes")
 	ErrInsufficientCapacity = errors.New("insufficient datanode capacity")
