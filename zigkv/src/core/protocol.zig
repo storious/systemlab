57cc27e5794ff4d2390c8c8b761dced98541d977
storious <2227563488@qq.com>
2026-07-01 14:22:48 +0800

    test(zigkv): add protocol tests

--- zigkv/src/core/protocol.zig
+++ zigkv/src/core/protocol.zig
@@ -11,3 +11,15 @@ pub fn parse(input: []const u8) ProtocolError!command.Command {
 pub fn formatError(allocator: std.mem.Allocator, err: anyerror) ![]u8 {
     return response.err(allocator, @errorName(err));
 }
+
+test "protocol parses command" {
+    const cmd = try parse("PING");
+    try std.testing.expect(cmd == .ping);
+}
+
+test "protocol formats error" {
+    const resp = try formatError(std.testing.allocator, command.ParseError.UnknownCommand);
+    defer std.testing.allocator.free(resp);
+
+    try std.testing.expectEqualStrings("-ERR UnknownCommand\r\n", resp);
+}
